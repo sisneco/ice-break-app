d00820b080890a408892a8bc95f072d3cab9bf04
sisneco <106042789+sisneco@users.noreply.github.com>
2023-06-11 13:26:14 +0900

    画面遷移のアニメーション処理修正

---
+++ components/NuxtLinkComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  link: String,
+  text: String,
+  icon: String,
+});
+
+const cStore = commonStore();
+
+const routingNow = () => {
+  if (props.link === useRoute().fullPath) {
+    return;
+  }
+
+  cStore.setIsAnimaiton(true);
+};
+</script>
+
+<template>
+  <NuxtLink
+    @click="routingNow()"
+    :to="link"
+    class="w-full h-full flex items-center justify-center"
+  >
+    <font-awesome-icon :icon="['fas', icon]" class="text-2xl" />
+    <span class="hidden md:inline md:pl-2 text-2xl font-serif">
+      {{ text }}
+    </span>
+  </NuxtLink>
+</template>
